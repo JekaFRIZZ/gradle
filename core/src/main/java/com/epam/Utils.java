c6ac05b45a0f6d8ee914d28603d5a646ea8bc725
Женя <lapko.eugene@gmail.com>
2021-09-23 22:17:10 +0300

    update Utils :core

--- core/src/main/java/com/epam/Utils.java
+++ core/src/main/java/com/epam/Utils.java
@@ -4,6 +4,9 @@ import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(String... str) {
+        if(str == null || str.length == 0) {
+            return false;
+        }
         for(String currentString :str) {
             if(!StringUtils.isPositiveNumber(currentString)) {
                 return false;
